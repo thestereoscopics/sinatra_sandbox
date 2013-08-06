4386831487b039ca81a1c7af3207fb501b4dd553
Jeremy Sell <thestereoscopics@gmail.com>
2013-08-06 14:06:17 -0700

    first and last commit, whoops

---
+++ sinatra_skeleton/app/controllers/index.rb
@@ -0,0 +1,22 @@
+get '/' do
+  # Look in app/views/index.erb
+  erb :index
+end
+
+get '/' do
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :post_cool_url
+end
